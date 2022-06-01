eb6e4d69ae9e77bdb36bcb1c374b2e68d347ecac
Kaeden Le <kaedenle@gmail.com>
2022-06-01 06:22:11 -0400

    Added the player class

--- Entity.py
+++ Entity.py
@@ -1,5 +1,6 @@
 import pygame, sys
 import random, math
+from Physics import Physics
 
 pygame.init()
 
@@ -12,16 +13,21 @@ class Entity(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.spritesheet = spritesheet
         self.flipped = False
+        
         self.state = None
+        #checks if state is different from last self.update_state call
+        self.queued_state = None
         self.dict_data = states
         
         self.a_timer = 0
         self.a_length = 0
         self.a_speed = 0
+        self.a_loop = False
         self.interrupt = False
         self.is_animating = False
         self.a_x = 0
         self.scaling = 1
+        self.physics = Physics(self.rect, [50, 50])
         
         if self.spritesheet == None:
             self.image.fill((255, 0, 0))
@@ -41,37 +47,89 @@ class Entity(pygame.sprite.Sprite):
         #render all white as transparent
         image.set_colorkey((0, 255, 0))
         return image
-    def a_loop(self):
+    def update_animation(self):
         if(self.is_animating):
             self.a_timer += self.a_speed
             #base case
             if(self.a_timer >= self.a_length):
                 self.is_animating = False
-                self.set_state("DEFAULT")
+                if(self.a_loop):
+                    self.is_animating = True
+                    self.a_timer = 0
                 return
             #get new frame
             self.image = self.get_image(self.a_x, int(self.a_timer), self.scaling)
     #----------------------------------STATE------------------------------------------
-    def set_state(self, state):
+    def update_state(self):
         #has the state changed (prevents a state from repeatedly resetting itself)
-        if(self.check_state(state)):
-            self.state = state
+        if(self.queued_state != self.state):
+            self.state = self.queued_state
             self.a_x = self.dict_data[self.state]["INDEX"]
             self.a_length = self.dict_data[self.state]["FRAME_LEN"]
-            self.inter = self.dict_data[self.state]["INTER"]
+            self.interrupt = self.dict_data[self.state]["INTER"]
+            self.a_timer = 0
+            self.is_animating = True
             
             if not ("SPEED" in self.dict_data[self.state]):
                 self.a_speed = 0.15
             else:
-                self.a_speed = self.dict_data[self.curState]["SPEED"]
-            self.a_timer = 0
-            self.is_animating = True
+                self.a_speed = self.dict_data[self.state]["SPEED"]
+            #Possibaly implement if loop is true then interrupt must be true
+            if not ("LOOP" in self.dict_data[self.state]):
+                self.a_loop = False
+            else:
+                self.a_loop = self.dict_data[self.state]["LOOP"]
             
-    def check_state(self, state):
-        #checks if new state is different from current state
-        if self.state != state:
+        #run the state function
+        if("FUNCT" in self.dict_data[self.state]):
+            self.dict_data[self.state]["FUNCT"](self)
+    def set_state(self, state):
+        #check if allowed to change state
+        if(self.is_animating == False or self.interrupt == True):
+            self.queued_state = state
+            #successful queue
             return True
+        #unsuccessful queue
         return False
-    def update(self):
+            
+    def update(self, dt):
         self.a_loop()
-    
+        self.physics.update(dt)
+
+class Player(Entity):
+    def __init__(self, w, h, states, controls, spritesheet = None):
+        Entity.__init__(self, w, h, states, spritesheet)
+        self.controls = controls
+    def default_state(self):
+        #will add differnt set_states for airborne players
+        self.set_state("DEFAULT")
+    def run_right(self):
+        self.physics.acc.x += self.physics.speed
+        self.flipped = False
+    def run_left(self):
+        self.physics.acc.x -= self.physics.speed
+        self.flipped = True
+    def events(self, event):
+        self.default_state()
+        #physics values
+        self.physics.acc.x = 0
+        self.physics.acc.y = self.physics.gravity
+        
+        #hold events
+        keys = pygame.key.get_pressed()
+        if keys[self.controls["MOVE_RIGHT"]]:
+            if(self.set_state("RUN_RIGHT")):
+                self.flipped = False
+        elif keys[self.controls["MOVE_LEFT"]]:
+            if(self.set_state("RUN_LEFT")):
+                self.flipped = True
+        #single press events
+        for e in event:
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_SPACE:
+                    self.set_state("SWING")
+    def update(self, dt, event):
+        self.update_animation()
+        self.events(event)
+        self.update_state()
+        self.physics.update(dt)
