f1829857ccc71cfce519f24cc4c862841f597eb1
Kaeden Le <kaedenle@gmail.com>
2022-06-01 06:22:35 -0400

    Added functionallity with the new player class

--- Main.py
+++ Main.py
@@ -1,9 +1,10 @@
 import pygame, sys
 import random, math
-from Entity import Entity
+from Entity import Entity, Player
 from Platform import Level, Platform
 from FileLookup import find
-from Dictionaries.Dictionary2 import PLAY_DICT
+from Dictionaries.Dictionary2 import STATE_DICT
+from Dictionaries.Controls_Dict import CONTROLS_DICT
 
 pygame.init()
 WINDOW_WIDTH = 832
@@ -24,26 +25,25 @@ TARGET_FPS = 60
 sprites = pygame.sprite.Group()
 textGroup = pygame.sprite.Group()
 filename = find("SS2(2)(64x64).png")
-print(PLAY_DICT["SWING"]["INDEX"])
+
 class Game:
     def __init__(self):
         self.level = Level(0, size)
         self.level.init()
-        self.Ent = Entity(64, 64, PLAY_DICT, filename)
-        sprites.add(self.Ent)
+        self.player = Player(64, 64, STATE_DICT, CONTROLS_DICT, filename)
+        sprites.add(self.player)
+        self.event = None
+        
     def events(self):
         self.event = pygame.event.get()
         for e in self.event:
             if e.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if e.type == pygame.KEYDOWN:
-                if e.key == pygame.K_SPACE:
-                    self.Ent.set_state("SWING")
     def update(self, dt):
-        self.Ent.update()
-        self.level.platList.draw(screen)
         self.events()
+        self.player.update(dt, self.event)
+        self.level.platList.draw(screen)
 
 game = Game()
 while True:      
