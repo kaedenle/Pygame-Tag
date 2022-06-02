99787431019c7b03f7b91cd95256b287b04e41a8
Kaeden Le <kaedenle@gmail.com>
2022-06-02 04:23:34 -0400

    Added the lock variable

--- Dictionaries/Dictionary2.py
+++ Dictionaries/Dictionary2.py
@@ -1,9 +1,6 @@
 #FOR SS2
 #---------------------------------POSSIBLE COMMANDS---------------------------------
-#INTER: What frame can you change state (actionable)?
-    #always (0)
-    #never (-1)
-    #Integer from [0, FRAME_LEN]
+#INTER: Can interrupt current state with anything?
 #FRAME_LEN: How many total frames is this animation?
 #INDEX: What row is the animation taken from the sprite sheet
 #BL: Prevents changing to listed states from current state
@@ -16,18 +13,9 @@
 #LOOP: Should the animation loop (TRUE) or stop after the first play (FALSE)?
     #default is TRUE
     #useless if RESET is TRUE or state isn't attatched to hold input
-#RESET: Once an animation loop finishes, should state be reset to default state (TRUE) or keep on current state (FALSE)
-    #Default is TRUE
-#FRAME: What frame from row should be picked if FRAME_LEN < 1
-    #Default is 0
-    #indexed
-#SPEED: How fast each frame should pass (0, 1]
-    #Default is 0.15
-    #closer to 0 is slowest, 1 is fastest, above 1 potentially skips frames
-    #Try to limit use of SPEED, try to build animation to fit the default of 0.15
 #FUNCT: Associated function to run (defined in class, will repeat every call)
-#CAN_MOVE: Can you move while in state
-    #Default is False
+#LOCK: Lock movement and turning
+    #default is TRUE
 STATE_DICT = {
 	"SWING":{
 	    "INTER":False,
@@ -47,27 +35,21 @@ STATE_DICT = {
 	    "INDEX":4,
             "BL":{"SWING2":True}
         },
-        "RUN_LEFT":{
+        "RUN":{
             "INTER":True,
 	    "FRAME_LEN":6,
 	    "INDEX":3,
             "LOOP": True,
             "BL":{"SWING2":True},
-            "FUNCT":lambda self: self.run_left()
-        },
-        "RUN_RIGHT":{
-            "INTER":True,
-	    "FRAME_LEN":6,
-	    "INDEX":3,
-            "LOOP": True,
-            "BL":{"SWING2":True},
-            "FUNCT":lambda self: self.run_right()
+            "FUNCT":lambda self: self.run(),
+            "LOCK": False
         },
         "JUMP":{
             "INTER":False,
 	    "FRAME_LEN":6,
 	    "INDEX":0,
-            "FUNCT":lambda self: self.jump()
+            "FUNCT":lambda self: self.jump(),
+            "LOCK": False
         }
 }
 
